898f7e48e01e7f42004522d16f9e64270e5b5dc5
Anshu Meena <68537640+anshumeena1947@users.noreply.github.com>
2020-12-02 06:46:41 +0530

    main files

---
+++ activator.py
@@ -0,0 +1,18 @@
+import os
+print("create by Anshu Meena By CodeNanshu")
+print(".\n.\n.\n.\n")
+print("Hello Windows Users")
+print("i help you to activate you Windwos")
+print(".\n.\n.\n.\n.\n.\n.\n.\n")
+print("please turn of your antivirus [like windows defender] its safe")
+ok = input("are you connect to the internet (y, n)")
+if ok == "y":
+    print("ok so now we start .\n.\n.\n.\n.\n.\n.\n.\n")
+if ok == "Y":
+     print("ok so now we start .\n.\n.\n.\n.\n.\n.\n.\n")
+else:
+    print("turn your data on .\n.\n.\n.\n")
+
+print("you windows is now going to activate")
+
+os.system("windows.cmd")
